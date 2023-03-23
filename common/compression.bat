70c44ca986894f560337ac0e1d60b23fcaf66fa6
wind <windtoogows@outlook.com>
2023-03-23 23:02:18 +0800

    打包和解压

--- common/compression.bat
+++ common/compression.bat
@@ -1,57 +1,21 @@
 @echo off
-@REM 打包专用命令
-@REM 只传一个参数，则为需要打包的文件夹名称
-@REM 传两个参数，则第一个为需要打包的文件夹名称，第二个为压缩包起始名称
-@REM 若要指定参数对象，-d 代表文件夹名称，-f代表压缩包名称
+
+@REM folder name
 set dirParam=dist
+@REM todo is file exist
+
+@REM file name
 set fileParam=dist
-@REM echo %dirParam%
 
-@REM if里面输出的是缓存？？，和语句外输出结果不一样
-if "%1%"=="-d" (
-    set dirParam=%2%
-    if "%3%"=="-f" (
-        set fileParam=%4%
-    )^
-    else  if not "%3%"=="" (
-        set fileParam=%3%
-    )
-)^
-else if "%1%"=="-f" (
-    set fileParam=%2%
-    if "%3%"=="-d" (
-        set dirParam=%4%
-    )^
-    else if not "%3%"=="" (
-        set dirParam=%3%
-    )
-)^
-else if not "%1%"=="" (
-    set dirParam=%1%
-    if "%2%"=="-f" (
-        set fileParam=%3%
-    )^
-    else if not "%2%"=="" (
-        set fileParam=%2%
-    )
-)^
-else (
-    echo 默认参数dist
-) 
-echo ----------
-echo 最终参数
-
-echo dirParam:%dirParam%
-echo fileParam:%fileParam%
-echo ----------
-
+@REM file suffix
 set/a h=%time:~0,2%,b=h+100
-@REM echo fileParam:"%~dp0%fileParam%%date:~0,4%%date:~5,2%%date:~8,2%%b:~-2%.zip"
-@REM "C:\Program Files\2345Soft\HaoZip\HaoZipc.exe" a -tzip "%~dp0%fileParam%%date:~0,4%%date:~5,2%%date:~8,2%%b:~-2%.zip" "%~dp0%dirParam%"
-"C:\Program Files\2345Soft\HaoZip\HaoZipc.exe" a -tzip "%cd%\%fileParam%%date:~0,4%%date:~5,2%%date:~8,2%%b:~-2%.zip" "%cd%\%dirParam%" -sn
+@REM how to keep date format ??
+set suffix=%fileParam%%date:~0,4%%date:~5,2%%date:~8,2%%b:~-2%
 
-echo 压缩%dirParam%完成
-@REM 打开文件夹
+tar -zcf %suffix%.tar.gz %dirParam%
+
+echo compress %dirParam% to %suffix% completely!
+
 start .
 
 echo %date% %time%
--- common/decompression.sh
+++ common/decompression.sh
@@ -3,42 +3,33 @@
 ###
 # @Author: zf
 # @Date: 2022-09-18 23:28:55
-# @LastEditTime: 2022-09-19 23:34:37
+# @LastEditTime: 2023-03-23 23:01:54
 # @LastEditors: zf
-# @Description: decompress zip file that starts with special name
+# @Description: decompress gz file that starts with special name
 ###
 
-# 解压文件夹名称
-folder=dist
-
+# 解压到文件夹路径
+folder="/dist"
 # 压缩包文件前缀
-prefix=pltest
-function updatePrefix() {
-    if [[ ! -n "$1" ]]; then
-        prefix=$1
-    fi
-}
+prefix="pltest"
 
-updatePrefix $1
-
-# remove folder before
+# 删除旧数据
 sudo rm -rf $folder
 
-filename=$(ls -lt | grep $prefix.*.zip | head -n 1 | awk '{print $9}')
-echo 准备解压$filename
-unzip -qO UTF-8 $filename
+filename=$(ls -lt | grep $prefix.*.tar.gz | head -n 1 | awk '{print $9}')
+echo "准备解压$filename"
+tar -zxf $filename -C $folder
 
-# unzip -q $filename -d $folder
+num=$(ls -l ./ | grep $prefix.*.tar.gz | wc -l | awk '{print $1}')
+echo "部署包个数$num"
 
-num=$(ls -l ./ | grep $prefix.*.zip | wc -l | awk '{print $1}')
-echo 部署包个数$num
-
-if [ $num -gt 2 ]; then
-    fileearly=$(ls -tr | grep $prefix.*.zip | head -n 1 | awk '{print $1}')
-    echo 删除$fileearly
+if [ $num -gt 3 ]; then
+    fileearly=$(ls -tr | grep $prefix.*.tar.gz | head -n 1 | awk '{print $1}')
+    echo "删除$fileearly"
     sudo rm -rf $fileearly
 else
     echo "无需删除文件"
 fi
 
-sudo rm -rf $folder
+# 查看gz包里的文件列表
+#  tar -ztf  $filename
