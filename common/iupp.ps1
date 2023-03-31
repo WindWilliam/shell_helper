6bd7f5c12a383727e2cc64229fb7f2675eb962f8
wind <windtoogows@outlook.com>
2023-04-01 02:34:46 +0800

    增加pnpm的在线安装/更新。

--- common/iupp.ps1
+++ common/iupp.ps1
@@ -1,28 +1,29 @@
 # pnpm的安装/更新命令
 # 单纯安装，需提前下载pnpm的安装包，才能进行安装/更新
 
-# 获取最新pnmp的安装包名称
+# 获取当前目录下最新的pnmp安装包
 $name = (ls * |where {$_.Name -like "pnpm*"} | sort LastWriteTime -desc | select -f 1).name
-
+# 是否存在pnmp安装包
 if ($null -eq $name) {
-    Write-Error "未找到pnpm的安装包！"
-    exit 1
-}
+    Write-Error "未找到pnpm的安装包，尝试在线安装"
 
-if ("pnpm.exe" -eq $name) {
-    # 安装/更新 ---- pnpm.exe是全局变量，故此处需要注意使用相对路径
-    Start-Process -FilePath .\pnpm.exe -ArgumentList "setup" -NoNewWindow -Wait -ErrorAction Continue
-    
-    Write-Host "安装/升级完成...`n" -ForegroundColor Green
-}else {
-    # 重命名，不然安装后文件名称不对
-    Rename-Item -Path $name -NewName pnpm.exe
+    iwr https://get.pnpm.io/install.ps1 -useb | iex
+} else {
+    if ("pnpm.exe" -eq $name) {
+        # 安装/更新 ---- pnpm.exe是全局变量，故此处需要注意使用相对路径
+        Start-Process -FilePath .\pnpm.exe -ArgumentList "setup" -NoNewWindow -Wait -ErrorAction Continue
 
-    # 安装/更新 ---- pnpm.exe是全局变量，故此处需要注意使用相对路径
-    Start-Process -FilePath .\pnpm.exe -ArgumentList "setup" -NoNewWindow -Wait -ErrorAction Continue
+        Write-Host "安装/升级完成...`n" -ForegroundColor Green
+    }else {
+        # 重命名，不然安装后文件名称不对
+        Rename-Item -Path $name -NewName pnpm.exe
 
-    # 还原重命名
-    Rename-Item -Path pnpm.exe -NewName $name
-    
-    Write-Host "安装/升级完成...`n" -ForegroundColor Green
+        # 安装/更新 ---- pnpm.exe是全局变量，故此处需要注意使用相对路径
+        Start-Process -FilePath .\pnpm.exe -ArgumentList "setup" -NoNewWindow -Wait -ErrorAction Continue
+
+        # 还原重命名
+        Rename-Item -Path pnpm.exe -NewName $name
+
+        Write-Host "安装/升级完成...`n" -ForegroundColor Green
+    }
 }
