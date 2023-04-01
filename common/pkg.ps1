23ef4b5e7c61542e66bb801b30ce4ddd76d9cb08
wind <windtoogows@outlook.com>
2023-04-02 02:14:29 +0800

    增加归档压缩命令。

---
+++ common/pkg.ps1
@@ -0,0 +1,16 @@
+# tar 归档压缩命令
+
+# 待压缩文件夹
+$folder = "dist"
+# 文件夹是否存在
+if (Test-Path -Path $folder) {
+    # 压缩后名称
+    $fileName = "result" + (Get-Date -Format "yyyyMMddHH") + ".tar.gz"
+
+    tar -zcf $fileName $folder
+
+    Write-Host $(Get-Date) " 压缩完成！`n" -ForegroundColor Green
+    start .
+} else {
+    Write-Error "$folder 文件夹不存在，压缩失败！"
+}
