afa4565c514b15156bcd671877ff07da6750b16a
wind <windtoogows@outlook.com>
2024-11-20 00:18:17 +0800

    完善ps1别名配置管理脚本。

--- common/addAlias.ps1
+++ common/addAlias.ps1
@@ -1,27 +1,118 @@
-# 增加全局别名命令  add-alias
+# 当前用户的PowerShell配置文件中的别名管理
 
+<# 获取输入值 #>
+function GetInput {
+    param (
+        [string]$prompt
+    )
+    $val = Read-Host $prompt
+    if ($val -eq "") {
+        Write-Host "输入不能为空，请重新输入！" -ForegroundColor Red
+        return GetInput -prompt $prompt
+    }
+    else {
+        return $val
+    }
+}
 
-$aliasName = "pp"
-$aliasValue = "pnpm"
+<# 检查别名是否存在 #>
+function CheckAliasExists {
+    param (
+        [string]$name
+    )
 
-# 别名是否存在
-if ( Get-Command $aliasName -ErrorAction SilentlyContinue ) {
-    Write-Warning "别名 $aliasName 已存在！"
-    exit 1
-} else {
-    # 确认是不是配置未生效
-    if ( Test-Path $profile ) {
-        # 是否已有内容
-        $now = Get-Content -Path $profile
-        if ( ($null -ne $now) -and ($now.contains("Set-Alias -Name $aliasName")) ) {
-            Write-Warning "别名 $aliasName 已存在别名配置中！"
-            exit 1
+    if ( Get-Command $name -ErrorAction SilentlyContinue ) {
+        Write-Warning "命令 $name 已存在！"
+        return $false
+    } 
+    elseif ( Get-Alias -Name $name -ErrorAction SilentlyContinue ) {
+        Write-Warning "别名 $name 已存在！"
+        return $false
+    }
+    else {
+        return $true
+    }
+}
+
+<# 获取别名的名称 #>
+function GetAliasName {
+    param (
+        [boolean]$check = $false
+    )
+
+    $name = GetInput -prompt "请输入别名的名称，例如：pp"
+    if(!$check) {
+        return $name
+    } else {
+        $ck = CheckAliasExists -name $name
+        if ($ck) {
+            return $name
+        }
+        else {
+            return GetAliasName
         }
     }
 }
 
-# 添加别名
-"Set-Alias -Name $aliasName $aliasValue" | Out-File -Append $profile
+<# 获取别名的值 #>
+function GetAliasValue {
+    $val = GetInput -prompt "请输入别名对应的值，例如：pnpm"
+    return $val
+}
 
-Write-Host "添加全局别名 $aliasName 成功！" -ForegroundColor Green
-Write-Host "若想验证效果，请在新的会话窗口中进行。`n" -ForegroundColor Green
+<# 添加别名 #>
+function AddAlias {
+    $name = GetAliasName -check $true
+    $val = GetInput -prompt "请输入别名对应的值，例如：pnpm"
+    # 添加别名
+    "# 添加别名 $name $val 于$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")`nSet-Alias -Name $name $val`n" | Out-File -Append $profile
+
+    Write-Host "`n全局别名 $name 添加成功！" -ForegroundColor Green
+}
+
+<# 删除别名 #>
+function DeleteAlias {
+    $name = GetAliasName
+
+    $now = Get-Content -Path $profile
+    # 别名注释行以及set-alias行
+    $updated = $now | Where-Object { $_ -notmatch "^# 添加别名 $name" -and $_ -notmatch "^Set-Alias -Name $name " }
+    # $updated = $now | Where-Object { $_ -notmatch " $name " }
+    Set-Content -Path $profile -Value $updated
+
+    Write-Host "`n全局别名 $name 删除成功！" -ForegroundColor Green
+}
+<# 注释别名 #>
+function CommentAlias {
+    $name = GetAliasName
+
+    $now = Get-Content -Path $profile
+    $updated = $now | ForEach-Object { if ($_ -match "^Set-Alias -Name $name ") { "# $_" } else { $_ } }
+    Set-Content -Path $profile -Value $updated
+
+    Write-Host "`n全局别名 $name 注释成功！" -ForegroundColor Green
+}
+
+
+<# ======开始====== #>
+Write-Host "欢迎使用PowerShell全局别名管理工具！" -ForegroundColor Green
+Write-Host "注意：别名不区分大小写。`n" -ForegroundColor Yellow
+
+# 根据输入参数选择处理模式--默认为add
+switch ($args[0]) {
+    "d" {
+        DeleteAlias
+    }
+    "c" {
+        CommentAlias
+    }
+    default {
+        AddAlias
+    }
+}
+
+# 重载配置文件--无效
+# . $profile
+
+Write-Host "请输入 . `$profile 以手动更新环境变量，或是重启PowerShell，然后再进行验证。" -ForegroundColor Yellow
+<# ======结束====== #>
